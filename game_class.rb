906ea0d2daa3da38da7f52e8dfcd74cf346db6d4
Andres <andres039039@hotmail.com>
2022-01-03 15:06:14 -0500

    implements players accessors and game methods for initial tests.

--- game_class.rb
+++ game_class.rb
@@ -1,11 +1,32 @@
+# This will need a .start method staying in the context.
 class Game
-  attr_reader :current_player
+  attr_accessor :current_player
   
   def initialize(player) 
     @current_player = player
   end
 
+  def compare_with_right_answer(right, user)
+    if right != user.to_i 
+      puts 'Nope!' 
+      current_player.lives = current_player.lives - 1
+    else puts 'Yeah!'
+    end
+  end
+  
+  
+
+  def check_lives
+    puts current_player.lives > 1 ? "#{current_player.name} wins! with a score of #{current_player.lives}/3" : 'next' 
+end
+  
   def ask_question
-    puts @current_player.name << ': What does 5 plus 3 equal?' 
+   number1 = rand(20)
+   number2 = rand(20)
+   result = number1 + number2
+   puts @current_player.name << ": What does #{number1} plus #{number2} equal?"
+   guess = gets.chomp
+    compare_with_right_answer(result, guess)
+    check_lives
   end
 end
--- main.rb
+++ main.rb
@@ -1,9 +1,13 @@
 require './game_class.rb'
 require './players_class.rb'
+require './game_flow.rb'
 
-player1 = Player.new('Player1', 3)
-player2 = Player.new('Player2', 3)
 
-test_game = Game.new(player2)
+# player1 = Player.new('Player1', 3)
+# player2 = Player.new('Player2', 3)
+# test_game = Game.new(player2)
 
-test_game.ask_question
+#test_game.ask_question
+#should happen on a loop, this file will probably just start.
+#puts test_game.current_player.lives
+test
