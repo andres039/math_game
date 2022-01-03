0695252039f0556c59ccafa95f79bf9aa0cf45b5
Andres <andres039039@hotmail.com>
2022-01-03 18:50:15 -0500

    fixes game-end, it now stops as soon as a player has no lives left

--- game_class.rb
+++ game_class.rb
@@ -5,7 +5,7 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @current_player = player1
+    @current_player = player2
   end
 
   def start
@@ -13,31 +13,32 @@ class Game
    end
 
   def ask_question
+    next_turn
     first_number = rand(20)
     second_number = rand(20)
     result = first_number + second_number
     puts "#{self.current_player.name}: what is #{first_number} plus #{second_number}?"
     user_answer = gets.chomp
     compare_with_right_answer(user_answer, result)
-    check_lives
+    
   end
 
   def compare_with_right_answer(user_answer, result)
     if user_answer.to_i == result 
       puts "good job! #{current_player.name} \n"
       puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3 \n ----- NEW TURN ----- \n"
-      next_turn
+      check_lives
       else 
         self.current_player.lives = self.current_player.lives - 1
-        next_turn
         puts "incorrect! \n "
         puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3 \n ----- NEW TURN ----- \n"
-        
+        check_lives
       end
   end
 
   def check_lives
-    self.current_player.lives < 1 ? end_game : ask_question
+   self.current_player.lives < 1 ? end_game 
+    : ask_question
   end
 
   def next_turn
